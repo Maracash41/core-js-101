a38b5184b089289247e24aa02194451b780ba7e8
Maracash <9531945@gmail.com>
2023-07-07 20:17:40 +0300

    feat: 25-03 Done!

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -444,8 +444,9 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, () => [])
+    .map((item, index) => Array.from({ length: n }, (v, i) => (i === index ? 1 : 0)));
 }
 
 /**
