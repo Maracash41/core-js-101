18c53ef5d41f02ea08cee34dc7f232b129b997c7
Maracash <9531945@gmail.com>
2023-07-09 18:11:58 +0300

    feat: 28-03 Done!

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -505,8 +505,16 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  const map = new Map();
+  array.map((item) => {
+    if (map.has(keySelector(item))) {
+      return map.get(keySelector(item))
+        .push(valueSelector(item));
+    }
+    return map.set(keySelector(item), [valueSelector(item)]);
+  });
+  return map;
 }
 
 /**
