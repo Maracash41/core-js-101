2c082263f821918df9684b1b985f9f6dbd028e50
Maracash <9531945@gmail.com>
2023-07-09 16:31:07 +0300

    feat: 26-03 Done!

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -7,7 +7,6 @@
  *                                                                                            *
  ******************************************************************************************** */
 
-
 /**
  * Returns an index of the specified element in array or -1 if element is not found
  *
@@ -38,7 +37,6 @@ function findElement(arr, value) {
 function generateOdds(len) {
   return Array.from({ length: len }, (_, i) => (i * 2) + 1);
 }
-
 
 /**
  * Returns the doubled array - elements of the specified array
@@ -119,7 +117,6 @@ function getUpperCaseStrings(arr) {
   return arr.map((item) => item.toUpperCase());
 }
 
-
 /**
  * Returns the array of string lengths from the specified string array.
  *
@@ -165,7 +162,6 @@ function getHead(arr, n) {
   return arr;
 }
 
-
 /**
  * Returns the n last items of the specified array
  *
@@ -179,7 +175,6 @@ function getHead(arr, n) {
 function getTail(arr, n) {
   return arr.splice(-n);
 }
-
 
 /**
  * Returns CSV representation of two-dimensional numeric array.
@@ -220,7 +215,6 @@ function toArrayOfSquares(arr) {
   return arr.map((item) => item ** 2);
 }
 
-
 /**
  * Transforms the numeric array to the according moving sum array:
  *     f[n] = x[0] + x[1] + x[2] +...+ x[n]
@@ -258,7 +252,6 @@ function getSecondItems(arr) {
   return arr.filter((item, index) => index % 2 !== 0);
 }
 
-
 /**
  * Propagates every item in sequence its position times
  * Returns an array that consists of: one first item, two second items, three third items etc.
@@ -276,7 +269,6 @@ function getSecondItems(arr) {
 function propagateItemsByPositionIndex(arr) {
   return arr.reduce((acc, item, index) => acc.concat(Array(index + 1).fill(item)), []);
 }
-
 
 /**
  * Returns the 3 largest numbers from the specified array
@@ -395,7 +387,6 @@ function toStringList(arr) {
   return arr.join();
 }
 
-
 /**
  * Sorts the specified array by country name first and city name
  * (if countries are equal) in ascending order.
@@ -462,8 +453,10 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const range = end - start;
+  const arr = Array.from({ length: range + 1 }, (_, i) => start + i);
+  return arr;
 }
 
 /**
@@ -515,7 +508,6 @@ function group(/* array, keySelector, valueSelector */) {
   throw new Error('Not implemented');
 }
 
-
 /**
  * Projects each element of the specified array to a sequence
  * and flattens the resulting sequences into one array.
@@ -533,7 +525,6 @@ function selectMany(/* arr, childrenSelector */) {
   throw new Error('Not implemented');
 }
 
-
 /**
  * Returns an element from the multidimensional array by the specified indexes.
  *
@@ -549,7 +540,6 @@ function selectMany(/* arr, childrenSelector */) {
 function getElementByIndexes(/* arr, indexes */) {
   throw new Error('Not implemented');
 }
-
 
 /**
  * Swaps the head and tail of the specified array:
@@ -572,7 +562,6 @@ function getElementByIndexes(/* arr, indexes */) {
 function swapHeadAndTail(/* arr */) {
   throw new Error('Not implemented');
 }
-
 
 module.exports = {
   findElement,
