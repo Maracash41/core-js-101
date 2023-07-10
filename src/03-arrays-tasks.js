a93e102d09019ce6bed55722b3169336105ba499
Maracash <9531945@gmail.com>
2023-07-10 17:50:17 +0300

    feat: 31-03 Done!

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -573,8 +573,11 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const halfLength = Math.trunc(arr.length / 2);
+  const tail = arr.splice(-halfLength);
+  const head = arr.splice(0, halfLength);
+  return tail.concat(arr, head);
 }
 
 module.exports = {
